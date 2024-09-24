a56f1145e7a663f524b6a0a7d07c2fc40acc50ce
gitcaesarm <153662044+gitcaesarm@users.noreply.github.com>
2024-09-24 14:41:05 +0000

    footer.js
    footer.js

---
+++ footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer bg-gray-800 text-white text-center p-4">
+      <p>&copy; 2024 Azubi Cloud Institute. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
